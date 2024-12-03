eb0b0e155ab81bb6dc680981b79c42e50b035153
Paul Uchenna <pauluchenna20@gmail.com>
2024-12-03 21:44:38 +0100

    eslint: ignoreDuringBuilds: true,

--- next.config.ts
+++ next.config.ts
@@ -23,6 +23,9 @@ const nextConfig: NextConfig = {
     deviceSizes: [640, 750, 1080, 1920, 2048],
     imageSizes: [16, 32, 48, 64, 128],
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
