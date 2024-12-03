dba51f408545afc5bde065145e4499266032133d
Paul Uchenna <pauluchenna20@gmail.com>
2024-12-03 20:53:10 +0100

    add hook: useCurrentLocale,   useChangeLocale,

--- locales/client.ts
+++ locales/client.ts
@@ -2,7 +2,13 @@
 "use client";
 import { createI18nClient } from "next-international/client";
 
-export const { useI18n, useScopedI18n, I18nProviderClient } = createI18nClient({
+export const {
+  useI18n,
+  useScopedI18n,
+  I18nProviderClient,
+  useCurrentLocale,
+  useChangeLocale,
+} = createI18nClient({
   en: () => import("./messages/en"),
   fr: () => import("./messages/fr"),
   es: () => import("./messages/es"),
