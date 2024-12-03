b9f60d5d319e32f5b0724c729e132d6e6ddc7388
Paul Uchenna <pauluchenna20@gmail.com>
2024-12-03 01:35:13 +0100

    Types for destination features

---
+++ src/Types/types.ts
@@ -0,0 +1,12 @@
+export type Destination = {
+  src: string;
+  title: string;
+  description: string;
+  link: string;
+};
+
+export type Feature = {
+  src: string;
+  title: string;
+  description: string;
+};
