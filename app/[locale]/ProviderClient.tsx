94cedb87ee616483d609bde01434d2ce6b2f02bc
Paul Uchenna <pauluchenna20@gmail.com>
2024-12-03 14:21:04 +0100

    change name of comp Provider To ProviverClient

--- app/[locale]/ProviderClient.tsx
+++ app/[locale]/ProviderClient.tsx
@@ -1,7 +1,7 @@
 import { I18nProviderClient } from "@/locales/client";
 import { PropsWithChildren } from "react";
 
-export function Providers(props: PropsWithChildren<{ locale: string }>) {
+export function ProviderClient(props: PropsWithChildren<{ locale: string }>) {
   return (
     <I18nProviderClient locale={props.locale}>
       {props.children}
