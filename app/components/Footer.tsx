8276f22dd9df9dcd1ba5cceb63150f12d6744ba3
Paul Uchenna <pauluchenna20@gmail.com>
2024-12-03 20:57:48 +0100

    remplace a tag by Link

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,5 +1,6 @@
 import { useI18n } from "@/locales/client";
 import React from "react";
+import Link from "next/link";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faFacebook,
@@ -17,32 +18,36 @@ export default function Footer() {
           <p>{t("footer.rights")}</p>
           <div className="space-x-6">
             {/* Social Links*/}
-            <a href="#" className="hover:text-gray-100">
+            <Link
+              target="_blank"
+              href="https://github.com/Paul-Uchenna"
+              className="hover:text-gray-100"
+            >
               <FontAwesomeIcon icon={faGithub} size="lg" />
-            </a>
-            <a href="#" className="hover:text-blue-500 transition-all">
+            </Link>
+            <Link href="#" className="hover:text-blue-500 transition-all">
               <FontAwesomeIcon icon={faFacebook} size="lg" />
-            </a>
-            <a href="#" className="hover:text-blue-500 transition-all">
+            </Link>
+            <Link href="#" className="hover:text-blue-500 transition-all">
               <FontAwesomeIcon icon={faTwitter} size="lg" />
-            </a>
-            <a href="#" className=" hover:text-red-500 transition-all">
+            </Link>
+            <Link href="#" className="hover:text-red-500 transition-all">
               <FontAwesomeIcon icon={faInstagram} size="lg" />
-            </a>
+            </Link>
           </div>
         </div>
 
         <div className="mt-4 text-center">
           <div className="space-x-6">
-            <a href="#" className="hover:text-gray-100">
+            <Link href="#" className="hover:text-gray-100">
               {t("footer.about")}
-            </a>
-            <a href="#" className="hover:text-gray-100">
+            </Link>
+            <Link href="#" className="hover:text-gray-100">
               {t("footer.contact")}
-            </a>
-            <a href="#" className="hover:text-gray-100">
+            </Link>
+            <Link href="#" className="hover:text-gray-100">
               {t("footer.terms")}
-            </a>
+            </Link>
           </div>
         </div>
       </div>
