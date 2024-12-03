1710bf2bc4cffddf76bf54aa69368ac3892eff40
Paul Uchenna <pauluchenna20@gmail.com>
2024-12-03 14:21:30 +0100

    Set up

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,28 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "plus.unsplash.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "unsplash.com",
+        pathname: "/**",
+      },
+    ],
+    minimumCacheTTL: 60,
+    deviceSizes: [640, 750, 1080, 1920, 2048],
+    imageSizes: [16, 32, 48, 64, 128],
+  },
 };
 
 export default nextConfig;
