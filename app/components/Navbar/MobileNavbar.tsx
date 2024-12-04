6802787e5c5132e4b4f9261bd766eda01fa5d584
Paul Uchenna <pauluchenna20@gmail.com>
2024-12-05 00:00:47 +0100

    Simplify handleScroll logic and optimize isVisible state management

--- app/components/Navbar/MobileNavbar.tsx
+++ app/components/Navbar/MobileNavbar.tsx
@@ -29,11 +29,8 @@ export default function MobileNavbar() {
   const handleScroll = useCallback(() => {
     const currentScrollY = window.scrollY;
 
-    if (currentScrollY > 10) setHasScrolled(true);
-    else setHasScrolled(false);
-
-    if (currentScrollY > lastScrollY) setIsVisible(false);
-    else setIsVisible(true);
+    setHasScrolled(currentScrollY > 10);
+    setIsVisible(currentScrollY <= lastScrollY || currentScrollY === 0);
 
     setLastScrollY(currentScrollY);
   }, [lastScrollY]);
@@ -53,7 +50,7 @@ export default function MobileNavbar() {
 
   return (
     <div className="md:hidden">
-      <section
+      <div
         className={`fixed top-0 left-0 right-0 w-full h-16 z-50 transition-transform duration-300 ${
           isVisible ? "translate-y-0" : "-translate-y-16"
         } ${hasScrolled ? "bg-black" : "bg-transparent"}`}
@@ -61,20 +58,24 @@ export default function MobileNavbar() {
         <div className="container mx-auto px-4 h-full">
           <div className="flex items-center justify-between h-full">
             <Logo />
-            <button type="button" onClick={toggleMenu}>
+            <button
+              type="button"
+              onClick={toggleMenu}
+              aria-label={isMenuOpen ? "Close Menu" : "Open Menu"}
+            >
               <Image
                 src={svg_open_Menu}
                 width={25}
                 height={25}
-                alt="Menu_Icon"
+                alt={isMenuOpen ? "Close Menu" : "Open Menu"}
               />
             </button>
           </div>
         </div>
-      </section>
+      </div>
 
       <div
-        className={`fixed top-0 left-0 right-0 bottom-0 bg-black/50 z-50 flex justify-end transition-all duration-300 ${
+        className={`fixed top-0 left-0 right-0 bottom-0 bg-black/50 z-50 flex justify-end transition-all duration-500 ease-in-out ${
           isMenuOpen
             ? "translate-x-0 opacity-100 visible"
             : "-translate-x-full opacity-0 invisible"
@@ -85,12 +86,13 @@ export default function MobileNavbar() {
             type="button"
             onClick={toggleMenu}
             className="absolute top-4 right-4 hover:-rotate-180 duration-500"
+            aria-label={isMenuOpen ? "Close Menu" : "Open Menu"}
           >
             <Image
               src={svg_close_menu}
               width={25}
               height={25}
-              alt="Menu_Icon"
+              alt={isMenuOpen ? "Close Menu" : "Open Menu"}
             />
           </button>
           <nav className="mb-6">
