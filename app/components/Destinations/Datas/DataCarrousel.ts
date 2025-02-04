269d01449e94062c3ff41c99134affcc3e13bf6f
Paul Uchenna <pauluchenna20@gmail.com>
2025-02-04 01:56:25 +0000

    move data courrousel into a new file

---
+++ app/components/Destinations/Datas/DataCarrousel.ts
@@ -0,0 +1,36 @@
+import { useScopedI18n } from "@/locales/client";
+
+export const dataCarrousel = () => {
+  return [
+    {
+      src: "https://images.unsplash.com/photo-1516483638261-f4dbaf036963?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      title: "Santorin, Grèce",
+      description:
+        "Maisons blanches accrochées aux falaises, où chaque coucher de soleil peint l'horizon en nuances de feu, révélant la magie d'une île méditerranéenne.",
+    },
+    {
+      src: "https://images.unsplash.com/photo-1523482580672-f109ba8cb9be?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      title: "Kyoto, Japon",
+      description:
+        "Temples ancestraux où la tradition danse avec la modernité, tandis que les cerisiers en fleurs dessinent un nuage éphémère de beauté japonaise.",
+    },
+    {
+      src: "https://images.unsplash.com/photo-1506929562872-bb421503ef21?q=80&w=1968&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      title: "Désert du Sahara, Maroc",
+      description:
+        "Océan de sable doré où le silence règne, les dunes ondulent sous un soleil implacable, et la nuit transforme le désert en un théâtre céleste.",
+    },
+    {
+      src: "https://images.unsplash.com/photo-1519659528534-7fd733a832a0?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      title: "Patagonie, Argentine",
+      description:
+        "Territoire sauvage où les montagnes escarpées et les glaciers millénaires racontent une histoire de nature primitif et de beauté indomptée.",
+    },
+    {
+      src: "https://images.unsplash.com/photo-1508672019048-805c876b67e2?q=80&w=1994&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      title: "Bora Bora, Polynésie Française",
+      description:
+        "Joyau du Pacifique où les bungalows sur pilotis flottent entre ciel et lagon, offrant un refuge de luxe au cœur d'un paradis tropical.",
+    },
+  ];
+};
