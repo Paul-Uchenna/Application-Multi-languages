1a805fe191d26a38ad0328cce4f047257ef418e1
Paul Uchenna <pauluchenna20@gmail.com>
2025-02-04 03:52:10 +0000

    add keyframe

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,9 +8,14 @@ export default {
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      animation: {
+        marquee: "marquee 20s linear infinite",
+      },
+      keyframes: {
+        marquee: {
+          "0%": { transform: "translateX(0%)" },
+          "100%": { transform: "translateX(-100%)" },
+        },
       },
     },
   },
